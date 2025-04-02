6d060d279a37ebf7a4d92389c2cba88483ae7ea7
Surya Teja <152494188+suryatejabatchu08@users.noreply.github.com>
2025-04-02 16:53:43 +0530

    Add files via upload

--- register_login/login.php
+++ register_login/login.php
@@ -11,7 +11,7 @@ if(isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DocHub - Login/Register</title>
-    <link rel="shortcut icon" href="assets/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../assets/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="../css/login.css">
 </head>
 <body>
